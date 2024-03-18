0b191fda77b57356843e07318da00fa18e009585
Afranio Dantas <afraniodantas0224@gmail.com>
2024-03-17 21:24:19 -0300

    temp: implamentation controller in api progress

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,12 @@
+// criando classe para acessar várias funções desse controller
+class UserController {
+/**
+ *  index - GET para listar vários registros
+ *  show - GET para exibir um registro específico
+ * create - POST para criar um registro
+ * update - PUT para atualizar um registro
+ * delete - DELETE para remover um registro
+ */
+}
+
+module.exports = UserController;
